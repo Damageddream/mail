493dafd7d93bf59751d3eefec773934866917906
unknown <marcan.grab@gmail.com>
2022-08-30 15:37:00 +0200

    cracked mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#inbox').addEventListener('click', () => {
+    load_mailbox('inbox')
+    fetch('/emails/inbox')
+    .then(response => response.json())
+    .then(emails =>{
+      console.log(emails);
+      for(let x in emails) {
+        const mail = emails[x];
+        document.querySelector('#emails-view').innerHTML=`Sender: ${mail.sender} <br> Subject: ${mail.subject} <br> Time: ${mail.timestamp}`;
+
+      }
+    
+    })  
+
+
+
+  });
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector("#compose-form").onsubmit = () => {
-    console.log("wewew")
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify({
@@ -19,7 +34,9 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(result => {
       console.log(result);
     })
+    load_mailbox('sent')
     return false;
+    
 
   }
 
@@ -45,7 +62,6 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
